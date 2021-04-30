669fb2c97985be7cd42e7798213b00f8e9d19616
JZSweet <sweet_zhaozhao@live.cn>
2021-04-29 18:24:05 -0700

    update

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,20 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "1",
-  database: "burgers_db"
-});
+var connection;
+if (process.env.JAWSDB_URL) {
+    // DB is JawsDB on Heroku
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    // DB is local on localhost
+    connection = mysql.createConnection({
+        port: 3306,
+        host: 'localhost',
+        user: 'test',
+        password: 'SweetZhao1968!',
+        database: 'burgers_db'
+    })
+};
 
 // Make connection.
 connection.connect(function(err) {
